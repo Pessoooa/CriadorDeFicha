08313e78209f301489bf724d165e7ed9122d3eb0
Pessoa <williampessowa@gmail.com>
2025-01-14 20:46:24 -0300

    Primeiros dados

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace CriadorDeFicha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Opcoes();
+        }
+    }
+}
+
