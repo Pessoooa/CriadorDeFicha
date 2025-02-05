8a1bf208802817bf9339ba587e37297374294bd1
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2025-02-05 00:27:51 -0300

    Ajuste de indentação

--- ComponenteDeFicha.cs
+++ ComponenteDeFicha.cs
@@ -1,24 +1,23 @@
-namespace testes.Classes
+namespace testes.Classes;
+
+public class ComponenteDeFicha
 {
-    public class ComponenteDeFicha
+    public string Nome { get; set; }
+    public Dictionary<string, int> Atributos { get; set; }
+
+    public int GetAtributo(string nome)
     {
-        public string Nome { get; set; }
-        public Dictionary<string, int> Atributos { get; set; }
+        return Atributos.ContainsKey(nome) ? Atributos[nome] : 0;
+    }
 
-        public int GetAtributo(string nome)
+    public void SetAtributo(string nome, int valor)
+    {
+        if (Atributos.ContainsKey(nome))
         {
-            return Atributos.ContainsKey(nome) ? Atributos[nome] : 0;
-        }
-
-        public void SetAtributo(string nome, int valor)
-        {
-            if (Atributos.ContainsKey(nome))
-            {
-                if (nome == "VIDA" && valor < 0)
-                    Atributos[nome] = 0;
-                else
-                    Atributos[nome] = valor;
-            }
+            if (nome == "VIDA" && valor < 0)
+                Atributos[nome] = 0;
+            else
+                Atributos[nome] = valor;
         }
     }
 }
